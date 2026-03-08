53a650bd32a08f7c74208c7e09964408acace3e2
zukachin <tswetha3188@gmail.com>
2026-03-08 13:24:20 +0530

    ensure folder exist

--- math-mentor/rag/vectorstore.py
+++ math-mentor/rag/vectorstore.py
@@ -1,7 +1,12 @@
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
+import os
 
 def get_vectorstore():
+    
+    # Ensure database folder exists
+    os.makedirs("chroma_db", exist_ok=True)
+
 
     embedding = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
